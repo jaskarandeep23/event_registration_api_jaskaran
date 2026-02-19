53c917a19f73924cdb51252b832eb07733002e53
Jaskarandeep Bhandol <jbhandol@rrc.ca>
2026-02-18 21:15:26 -0700

    feat: add Firebase Firestore configuration

---
+++ config/firebase.ts
@@ -0,0 +1,10 @@
+import admin from "firebase-admin";
+import { getFirestore } from "firebase-admin/firestore";
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.applicationDefault(),
+  });
+}
+
+export const db = getFirestore();
