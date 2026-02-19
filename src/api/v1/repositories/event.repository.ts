c5d2c84e4b829778dbae8e9e803294f8946a9922
Jaskarandeep Bhandol <jbhandol@rrc.ca>
2026-02-18 21:21:22 -0700

    feat: implement Event repository for Firestore operations

---
+++ src/api/v1/repositories/event.repository.ts
@@ -0,0 +1,35 @@
+import { db } from "../../../config/firebase";
+import { Event } from "../models/event.model";
+
+const collection = db.collection("events");
+
+export const createEvent = async (event: Event): Promise<Event> => {
+  const docRef = await collection.add(event);
+  return { id: docRef.id, ...event };
+};
+
+export const getAllEvents = async (): Promise<Event[]> => {
+  const snapshot = await collection.get();
+
+  return snapshot.docs.map((doc) => {
+    const data = doc.data() as Event;
+
+    return {
+      id: doc.id,
+      ...data,
+    };
+  });
+};
+
+export const getEventById = async (id: string): Promise<Event | null> => {
+  const doc = await collection.doc(id).get();
+
+  if (!doc.exists) return null;
+
+  const data = doc.data() as Event;
+
+  return {
+    id: doc.id,
+    ...data,
+  };
+};
