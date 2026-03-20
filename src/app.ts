1660b2e30efd210e17daea09817db360833053e2
Jaskarandeep Bhandol <jbhandol@rrc.ca>
2026-03-20 14:09:50 -0600

    feat: add custom helmet security configuration

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,21 @@
 import express from "express";
 import eventRoutes from "./api/v1/routes/event.routes";
 import healthRoutes from "./api/v1/routes/health.routes";
+import helmet from "helmet";
 
 const app = express();
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+    frameguard: { action: "deny" },
+    hsts: {
+      maxAge: 31536000,
+      includeSubDomains: true,
+    },
+    noSniff: true,
+    referrerPolicy: { policy: "no-referrer" },
+  })
+);
 
 app.use(express.json());
 
